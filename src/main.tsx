6ae84c52bc8395a6c6cb43921f5c660f8fedc244
Leesa Ward <leesa@doubleedesign.com.au>
2026-04-30 12:54:10 +1000

    Fix: Load my common styling into the UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import '@doubleedesign/doublee-site-style/doublee-tokens.css';
+import '@doubleedesign/doublee-site-style/doublee-common.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
