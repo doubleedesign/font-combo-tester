3e8bf08900e77bbd794aca3edca84c08b65c9bd5
Leesa Ward <leesa@doubleedesign.com.au>
2026-04-30 10:23:01 +1000

    Refactor(CssContext): Moved font file into common style package; simplify accordingly

--- src/controllers/CssContext/CssContext.tsx
+++ src/controllers/CssContext/CssContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useCallback, useContext, type ReactNode, useEffect } from 'react';
+import { createContext, useState, useCallback, useContext, type ReactNode } from 'react';
+import fonts from '@doubleedesign/doublee-site-style/doublee-fonts.css?raw';
 import tokens from '@doubleedesign/doublee-site-style/doublee-tokens.css?raw';
 import styles from '@doubleedesign/doublee-site-style/doublee-common.css?raw';
 import { tidyCss } from '../../utils.ts';
@@ -27,29 +28,11 @@ const CssContext = createContext<CssContextValue>({
 
 export const CssProvider = ({ children }: { children: ReactNode }) => {
 	const [content, setContent] = useState({
-		fonts: '',
-		tokens: '',
-		styles: ''
+		fonts,
+		tokens: tidyCss(tokens),
+		styles: tidyCss(styles)
 	});
 	const [originalContent] = useState(content);
-
-	const fetchDefaultCss = useCallback(async () => {
-		const fonts = await fetch('https://doublee.dev/fonts/fonts.css')
-			.then(res => {
-				return res.text();
-			}).then(data => {
-				return data;
-			});
-
-		// Don't import the font-face rules twice when this data is used to inject styles into the preview
-		const modifiedTokensFile = tokens.replace('@import \'https://doublee.dev/fonts/fonts.css\';', '');
-
-		setContent({
-			fonts,
-			tokens: tidyCss(modifiedTokensFile),
-			styles: tidyCss(styles)
-		});
-	}, []);
 
 	const setCss = useCallback((key: CssFileType, content: string) => {
 		setContent(prev => ({
@@ -64,10 +47,6 @@ export const CssProvider = ({ children }: { children: ReactNode }) => {
 			[key]: originalContent[key]
 		}));
 	}, [originalContent]);
-
-	useEffect(() => {
-		fetchDefaultCss().then();
-	}, [fetchDefaultCss]);
 
 	const value = {
 		css: content,
